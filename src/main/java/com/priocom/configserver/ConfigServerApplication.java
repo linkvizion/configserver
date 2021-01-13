838518e3f35a638009950e4b84819ad9997891e3
snebesnyi <serhii.nebesnyi@gmail.com>
2021-01-13 15:18:47 +0200

    .fc

---
+++ src/main/java/com/priocom/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.priocom.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+@EnableConfigServer
+public class ConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+}
